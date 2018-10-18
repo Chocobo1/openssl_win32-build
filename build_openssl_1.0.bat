86c28421d2446d0d1a1c3df07cae4d7cde5f4d5b
Chocobo1 <Chocobo1@users.noreply.github.com>
2018-10-18 16:00:15 +0800

    Build openssl 1.0

---
+++ build_openssl_1.0.bat
@@ -0,0 +1,18 @@
+@echo off
+rem Place this file at the root of the src lib
+
+if not defined CL_EXIST (
+	call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+	set CL_EXIST=1
+)
+
+set OPENSSL_X64_DIR=c:\openssl_64
+
+perl Configure VC-WIN64A no-shared --prefix="%OPENSSL_X64_DIR%"
+call ms\do_win64a
+powershell -Command "& { (Get-Content 'makefile') | ForEach-Object { $_ -replace '/MT', '/MT /Zc:wchar_t-' } | ForEach-Object { $_ -replace '/debug', '/debug /dynamicbase /nxcompat /incremental:no' } |  Set-Content 'makefile' }"
+
+nmake -f "ms\nt.mak" install
+
+copy "tmp32\lib.pdb" "%OPENSSL_X64_DIR%\lib"
+copy "tmp32\app.pdb" "%OPENSSL_X64_DIR%\lib"
