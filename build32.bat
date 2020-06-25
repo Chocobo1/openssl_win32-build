bda5ab27cb0a16b6127d3116f05f44b12de34c3c
Chocobo1 <Chocobo1@users.noreply.github.com>
2020-06-25 12:42:11 +0800

    Update scripts

--- build32.bat
+++ build32.bat
@@ -1,14 +1,14 @@
 @echo off
 rem Place this file at the root of the src lib
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64_x86
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64_x86
 
 set OPENSSL_X32_DIR=c:\openssl_32
 
 mkdir _build && cd _build
 
-perl ..\Configure VC-WIN32 no-shared --prefix="%OPENSSL_X32_DIR%" --openssldir="%OPENSSL_X32_DIR%\ssl"
-powershell -Command "& { (Get-Content 'makefile') | ForEach-Object { $_ -replace '/MT', '/MT /Zc:wchar_t-' } | ForEach-Object { $_ -replace '/debug', '/debug /dynamicbase /nxcompat /incremental:no' } |  Set-Content 'makefile' }"
+C:\Strawberry\perl\bin\perl ..\Configure VC-WIN32 no-shared --prefix="%OPENSSL_X32_DIR%" --openssldir="%OPENSSL_X32_DIR%\ssl"
+powershell -Command "& { (Get-Content 'makefile') | ForEach-Object { $_ -replace '/debug', '/debug /opt:ref /opt:icf /incremental:no' } |  Set-Content 'makefile' }"
 
 nmake install
 
--- build64.bat
+++ build64.bat
@@ -1,14 +1,14 @@
 @echo off
 rem Place this file at the root of the src lib
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 
 set OPENSSL_X64_DIR=c:\openssl_64
 
 mkdir _build && cd _build
 
-perl ..\Configure VC-WIN64A no-shared --prefix="%OPENSSL_X64_DIR%" --openssldir="%OPENSSL_X64_DIR%\ssl"
-powershell -Command "& { (Get-Content 'makefile') | ForEach-Object { $_ -replace '/MT', '/MT /Zc:wchar_t-' } | ForEach-Object { $_ -replace '/debug', '/debug /dynamicbase /nxcompat /incremental:no' } |  Set-Content 'makefile' }"
+C:\Strawberry\perl\bin\perl ..\Configure VC-WIN64A no-shared --prefix="%OPENSSL_X64_DIR%" --openssldir="%OPENSSL_X64_DIR%\ssl"
+powershell -Command "& { (Get-Content 'makefile') | ForEach-Object { $_ -replace '/debug', '/debug /opt:ref /opt:icf /incremental:no' } |  Set-Content 'makefile' }"
 
 nmake install
 
